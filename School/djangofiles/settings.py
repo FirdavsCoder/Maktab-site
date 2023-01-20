e3c8f4ca41a01f33320f8e31fd7db287ff8ddf38
Firdavs Yorkulov <113686185+FirdavsCoder@users.noreply.github.com>
2023-01-20 23:36:20 +0500

    updated

--- School/djangofiles/settings.py
+++ School/djangofiles/settings.py
@@ -117,7 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATICFILES_DIRS = [
+    str(BASE_DIR.joinpath('static'))
+]
+STATIC_ROOT = str(BASE_DIR.joinpath('dizayn'))
 
 MEDIA_URL = 'media/'
 MEDIAFILES_DIRS = os.path.join(BASE_DIR,'')
